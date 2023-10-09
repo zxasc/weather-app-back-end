664d4a90dc59573b19c49ac5adaf41feb73f52fc
Paweł <55333847+zxasc@users.noreply.github.com>
2023-10-10 01:00:58 +0200

    change cors origin

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.listen(3000);
 const apiKey = process.env.API_KEY;
 
 
-app.get('/:city', cors({ origin: 'http://magical-lamington-58153c.netlify.app' }), (req, res) => {
+app.get('/:city', cors({ origin: 'https://magical-lamington-58153c.netlify.app' }), (req, res) => {
     const cityInput = req.params.city;
     var name;
 
